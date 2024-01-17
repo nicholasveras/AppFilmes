89cb3a0d4ba19ba0e5c50a88c188ebe891db7cc2
Nicholas Veras <nicholasveras@MacBook-Air-de-Nicholas.local>
2024-01-17 13:01:40 -0300

    feat: modal finished

--- src/Modal/index.jsx
+++ src/Modal/index.jsx
@@ -1,14 +1,59 @@
 import React from "react";
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 export default function Detalhes(){
   return(
-    <View>
-      <Text>Detalhes do Filme</Text>
+    <View style={styles.container}>
+      <View style={styles.modalContainer}>
+        <TouchableOpacity style={styles.btnVoltar}>
+          <Text style={{color: '#FFF', fontSize: 16}}>Voltar</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.titulo}>Vingadores Ultimato</Text>
+        <Text style={styles.sinopse}>Sinopse:</Text>
+        <Text style={styles.descricao}>Aqui vai a sinopse desse filme que voce clicou!</Text>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    marginLeft: 10,
+    marginRight: 10,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end', 
+  },
+  modalContainer: {
+    //width: '90%',
+    height: '80%',
+    backgroundColor: '#121212',
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+  },
+  btnVoltar: {
+    backgroundColor: '#E52246',
+    padding: 10,
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+  },
+  titulo: {
+    textAlign: 'center',
+    color: '#FFF',
+    padding: 10,
+    fontSize: 28,
+    fontWeight: 'bold',
+  },
+  sinopse: {
+    color: '#FFF',
+    fontSize: 18,
+    marginBottom: 8,
+    marginLeft: 10
+  },
+  descricao: {
+    color: '#FFF',
+    marginLeft: 10,
+    marginRight: 10,
+  }
 });
